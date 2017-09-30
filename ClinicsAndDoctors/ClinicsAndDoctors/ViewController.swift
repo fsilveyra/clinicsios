76e861544cb274f9fa2c507cc889dc0302fb967b
risalgue <risalgue@gmx.com>
2017-09-30 12:27:18 -0400

    Uploading Files for Login and Register View

---
+++ ClinicsAndDoctors/ClinicsAndDoctors/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClinicsAndDoctors
+//
+//  Created by Reinier Isalgue on 28/09/17.
+//  Copyright © 2017 InfinixSoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
