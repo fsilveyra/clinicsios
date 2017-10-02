0b9caa7c58d112b5c4e2dbce1824e7640a3cc675
osmely <osmelyf@gmail.com>
2017-10-02 09:59:10 -0400

    Icono y ajustes

--- ClinicsAndDoctors/ClinicsAndDoctors/VC/LoginWithEmailVC.swift
+++ ClinicsAndDoctors/ClinicsAndDoctors/VC/LoginWithEmailVC.swift
@@ -27,7 +27,10 @@ class LoginWithEmailVC: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 
     /*
     // MARK: - Navigation
