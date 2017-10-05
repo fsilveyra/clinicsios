2bb478fca5374c5603f1054e3491c98bae79f630
risalgue <risalgue@gmx.com>
2017-10-05 13:55:55 -0400

    Change for GoogleMaps

---
+++ ClinicsAndDoctors/ClinicsAndDoctors/VC/Cell/CustomInfoVIew.swift
@@ -0,0 +1,21 @@
+//
+//  CustomInfoVIew.swift
+//  ClinicsAndDoctors
+//
+//  Created by Reinier Isalgue on 05/10/17.
+//  Copyright © 2017 InfinixSoft. All rights reserved.
+//
+
+import UIKit
+
+class CustomInfoVIew: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ ClinicsAndDoctors/ClinicsAndDoctors/VC/Cell/MapInfoClinicVC.swift
@@ -0,0 +1,35 @@
+//
+//  MapInfoClinicVC.swift
+//  ClinicsAndDoctors
+//
+//  Created by Reinier Isalgue on 05/10/17.
+//  Copyright © 2017 InfinixSoft. All rights reserved.
+//
+
+import UIKit
+
+class MapInfoClinicVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
