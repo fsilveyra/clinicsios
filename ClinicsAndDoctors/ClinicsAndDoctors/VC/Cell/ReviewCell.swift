d1e93121576accd982e024d6564be6b188977be8
risalgue <risalgue@gmx.com>
2017-10-08 23:18:37 -0400

    Uploading ReviewVC, DetailVC change and FavoriteVC

---
+++ ClinicsAndDoctors/ClinicsAndDoctors/VC/Cell/ReviewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ReviewCell.swift
+//  ClinicsAndDoctors
+//
+//  Created by Reinier Isalgue on 08/10/17.
+//  Copyright © 2017 InfinixSoft. All rights reserved.
+//
+
+import UIKit
+
+class ReviewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
