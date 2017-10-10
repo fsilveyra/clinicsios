67243099fc7c390464c9e453ac6175627e83518a
risalgue <risalgue@gmail.com>
2017-10-10 15:00:41 -0400

    updating images @2x

---
+++ ClinicsAndDoctors/ClinicsAndDoctors/VC/Cell/ClinicCell.swift
@@ -0,0 +1,24 @@
+//
+//  ClinicCell.swift
+//  ClinicsAndDoctors
+//
+//  Created by reinier on 10/10/17.
+//  Copyright © 2017 InfinixSoft. All rights reserved.
+//
+
+import UIKit
+
+class ClinicCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
