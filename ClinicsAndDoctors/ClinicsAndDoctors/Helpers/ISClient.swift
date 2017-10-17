d9b1ba51cf5eb82e79dd72c292cba6b0a5979dbf
risalgue <risalgue@gmail.com>
2017-10-16 21:24:18 -0400

    Uploading Class of Model

---
+++ ClinicsAndDoctors/ClinicsAndDoctors/Helpers/ISClient.swift
@@ -0,0 +1,13 @@
+//
+//  ISClient.swift
+//  ClinicsAndDoctors
+//
+//  Created by reinier on 14/10/17.
+//  Copyright © 2017 InfinixSoft. All rights reserved.
+//
+
+import UIKit
+
+class ISClient: NSObject {
+
+}
---
+++ ClinicsAndDoctors/ClinicsAndDoctors/Models/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  ClinicsAndDoctors
+//
+//  Created by reinier on 14/10/17.
+//  Copyright © 2017 InfinixSoft. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+
+}
