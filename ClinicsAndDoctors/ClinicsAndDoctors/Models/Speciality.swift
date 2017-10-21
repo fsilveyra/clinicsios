de665d45766aea5cc20707f6a1cdbe490f3b78ba
risalgue <risalgue@gmail.com>
2017-10-21 17:34:01 -0400

    Uploading new CLass

---
+++ ClinicsAndDoctors/ClinicsAndDoctors/Models/Speciality.swift
@@ -0,0 +1,13 @@
+//
+//  Speciality.swift
+//  ClinicsAndDoctors
+//
+//  Created by reinier on 20/10/17.
+//  Copyright © 2017 InfinixSoft. All rights reserved.
+//
+
+import UIKit
+
+class Speciality: NSObject {
+
+}
